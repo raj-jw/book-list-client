d6d44f8a773b5f4d83cebd8ab0b83e54f5477ecd
ria agnes jose <joseagnesria@gmail.com>
2018-05-23 11:10:55 -0700

    added book title,and book image,also corrected error callback

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -4,7 +4,7 @@ var app = app || {};
 
 (function(module) {
   function errorCallback(err) {
-    console.error(err);
+    console.error('this is the error we care about',err);
     module.errorView.initErrorPage(err);
   }
 
@@ -13,14 +13,14 @@ var app = app || {};
     this.title = bookObject.title;
     this.isbn10 = bookObject.isbn_10;
     this.isbn13 = bookObject.isbn_13;
-   this.imageUrl = bookObject.image_url;
+   this.imageUrl = bookObject.img_url;
    this.description = bookObject.description;
     // Book.keys(bookObject).forEach(key => this[key]= bookObject[key]);
   }
 
   Book.prototype.toHtml = function() {
     let template = Handlebars.compile($('#book-list-template').text());
-    return app.render('.container', this);
+    return app.render('book-list-template', this);
     
   }
 
